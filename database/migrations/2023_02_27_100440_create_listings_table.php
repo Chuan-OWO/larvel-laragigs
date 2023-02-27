501ad0c364b96b911d19d9b3aca6c8881da176e8
chuan <101337985+Chuan-OWO@users.noreply.github.com>
2023-02-27 18:18:19 +0800

    建立 mysql table col

--- database/migrations/2023_02_27_100440_create_listings_table.php
+++ database/migrations/2023_02_27_100440_create_listings_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('tags');
+            $table->string('company');
+            $table->string('email');
+            $table->string('website');
+            $table->string('description');
             $table->timestamps();
         });
     }
